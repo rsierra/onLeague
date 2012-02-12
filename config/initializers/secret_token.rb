b471119dbecaddda51efe8771e6e686d7ef1b937
Ruben Sierra <maguilag@gmail.com>
2012-02-12 21:37:26 +0100

    Initial rails project.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+OnLeague::Application.config.secret_token = 'c4139200118a4571cc39c16a02849d4c460fa1cd9e0920cc3e967b215656af6fc8415a0a9c16401b61f154eacde13b27b461e4cde271f1c9eaa174795cde49ba'
