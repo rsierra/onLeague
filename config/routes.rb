15205f76ef4ef6040d054cb102c2a435de594cbb
Ruben Sierra <maguilag@gmail.com>
2012-02-15 00:25:32 +0100

    Added initial user authentication.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 OnLeague::Application.routes.draw do
+  devise_for :users
+
   root :to => "home#index"
 
   # The priority is based upon order of creation:
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
@@ -5,3 +6,15 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+(1..20).each do |n|
+  user_params =
+  {
+    :email => "user#{n}@mail.com",
+    :password => n.to_s * 6,
+    :password_confirmation => n.to_s * 6
+  }
+  user = User.new user_params
+  user.skip_confirmation! # Sets confirmed_at to Time.now, activating the account
+  user.save
+end
