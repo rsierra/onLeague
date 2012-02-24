59b0beb4fe5b7a47ad1c6afb2df7839dcfe9e629
Ruben Sierra <maguilag@gmail.com>
2012-02-24 01:56:41 +0100

    New controller action to delete oath providers.

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -12,6 +12,17 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     create request.env["omniauth.auth"]
   end
 
+  def delete
+    provider = params[:provider]
+    if current_user && current_user.has_provider?(provider)
+      current_user.oauth_providers.where(:provider => provider).first.destroy
+      redirect_to edit_user_registration_path
+    else
+      flash[:alert] = t('devise.omniauth_callbacks.cant_delete', :provider => provider)
+      redirect_to edit_user_registration_path
+    end
+  end
+
   private
 
   def create(omniauth)
