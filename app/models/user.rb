a90e19fc4afd51f735696687a608f07beceb519e
Ruben Sierra <maguilag@gmail.com>
2012-02-26 23:51:53 +0100

    Validated user name presence.

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@ class User < ActiveRecord::Base
 
   has_many :oauth_providers, :dependent => :destroy
 
+  validate :name, :presence => true
+
   # Include default devise modules. Others available are:
   # :token_authenticatable, :encryptable, :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
