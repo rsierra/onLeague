3dd210b1434b57214991822bafe7d5e9dadf0d22
Ruben Sierra <maguilag@gmail.com>
2012-03-17 00:29:42 +0100

    Added league to session.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,14 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  before_filter :set_league
+
+  def set_league
+    if League.active.count > 0
+      session[:league_id] ||= League.active.first.id
+      @league = League.active.exists?(session[:league_id]) ? League.active.find(session[:league_id]) : League.active.first
+    else
+      @league = League.new # If there aren´t leagues, we create an empty one to avoid errors
+    end
+  end
 end
