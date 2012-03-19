4a6569d442c430a7798afdf4005e5d53b9507e5a
Ruben Sierra <maguilag@gmail.com>
2012-03-19 01:50:05 +0100

    Club model updated with translate and description removed.

--- app/models/club.rb
+++ app/models/club.rb
@@ -1,6 +1,11 @@
 class Club < ActiveRecord::Base
   has_and_belongs_to_many :leagues
 
+  translates :description
+  has_many :club_translations, :dependent => :destroy
+  accepts_nested_attributes_for :club_translations
+  validates_associated :club_translations
+
   extend FriendlyId
   friendly_id :name, use: :slugged
 
@@ -9,4 +14,14 @@ class Club < ActiveRecord::Base
   validates :short_name,  :presence => true,
                           :length => { :maximum => 3 }
   validates :slug,  :presence => true, :uniqueness => true
+
+  after_create :default_values
+
+  private
+
+  def default_values
+    I18n.available_locales.each do |locale|
+      club_translations << ClubTranslation.create({ :locale => locale }) unless club_translations.exists?(:locale => locale)
+    end
+  end
 end
