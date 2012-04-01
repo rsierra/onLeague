5dc29de56186bf7deecededce087fbdaab531bfd
Ruben Sierra <maguilag@gmail.com>
2012-04-01 22:36:43 +0200

    Added ClubFile model.

---
+++ app/models/club_file.rb
@@ -0,0 +1,26 @@
+class ClubFile < ActiveRecord::Base
+  POSITIONS = %w(goalkeeper defender midfielder forward)
+
+  belongs_to :club
+  belongs_to :player
+
+  include Enumerize
+  enumerize :position, :in => POSITIONS
+
+  validates :club_id, :presence => true
+  validates :player_id, :presence => true,
+    :uniqueness => { :scope => [:club_id, :season_in, :week_in], :message => :only_one_player }
+  validates :number, :presence => true, :numericality => { :only_integer => true }
+  validates :position, :presence => true, :inclusion => { :in => POSITIONS }
+  validates :value, :presence => true, :numericality => true
+  validates :week_in, :presence => true, :numericality => { :only_integer => true }
+  validates :season_in, :presence => true, :numericality => { :only_integer => true }
+
+  def position_enum
+    ClubFile.position.values
+  end
+
+  def custom_label_method
+    "#{player.name} (#{club.name})" unless player.blank? || club.blank?
+  end
+end
