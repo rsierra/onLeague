eafaa9b6fffbcb4abcb813807da46f0068871f25
Ruben Sierra <maguilag@gmail.com>
2012-04-01 18:16:03 +0200

    Added player model.

---
+++ app/models/player.rb
@@ -0,0 +1,12 @@
+class Player < ActiveRecord::Base
+  belongs_to :country
+
+  extend FriendlyId
+  friendly_id :name, use: :slugged
+
+  validates :name, :presence => true, :length => { :maximum => 64 }
+  validates :born, :presence => true
+  validates :active, :inclusion => { :in => [true, false] }
+  validates :country_id, :presence => true
+  validates :slug,  :presence => true, :uniqueness => true
+end
