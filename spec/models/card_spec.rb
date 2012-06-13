579fd77af83586e563add0549735c58655244072
Ruben Sierra <maguilag@gmail.com>
2012-06-13 20:47:07 +0200

    Removed minute validations specs from cards.

--- spec/models/card_spec.rb
+++ spec/models/card_spec.rb
@@ -11,43 +11,6 @@ describe Card do
       its(:player_relation) { should eql :player }
     end
 
-    context "without minute" do
-      let(:card) { build(:card, minute: nil) }
-      subject { card }
-
-      it { should_not be_valid }
-      it { should have(2).error_on(:minute) }
-      it { card.error_on(:minute).should include I18n.t('errors.messages.blank') }
-      it { card.error_on(:minute).should include I18n.t('errors.messages.not_a_number') }
-    end
-
-    context "with minute less than 0" do
-      let(:card) { build(:card, minute: -1) }
-      subject { card }
-
-      it { should_not be_valid }
-      it { should have(1).error_on(:minute) }
-      it { card.error_on(:minute).should include I18n.t('errors.messages.greater_than_or_equal_to', :count => 0) }
-    end
-
-    context "with minute greater than 130" do
-      let(:card) { build(:card, minute: 131) }
-      subject { card }
-
-      it { should_not be_valid }
-      it { should have(1).error_on(:minute) }
-      it { card.error_on(:minute).should include I18n.t('errors.messages.less_than_or_equal_to', :count => 130) }
-    end
-
-    context "with not integer minute" do
-      let(:card) { build(:card, minute: 10.1) }
-      subject { card }
-
-      it { should_not be_valid }
-      it { should have(1).error_on(:minute) }
-      it { card.error_on(:minute).should include I18n.t('errors.messages.not_an_integer') }
-    end
-
     context "without kind" do
       let(:card) { build(:card, kind: nil) }
       subject { card }
