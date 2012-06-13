7e3a40e51009ad6322008bd43ada186898c97434
Ruben Sierra <maguilag@gmail.com>
2012-06-13 20:16:53 +0200

    New specs for second player in game event.

--- spec/models/extensions/game_event_spec.rb
+++ spec/models/extensions/game_event_spec.rb
@@ -11,7 +11,9 @@ describe Extensions::GameEvent do
         include Extensions::GameEvent
         acts_as_game_event
       end
-      I18n.backend.store_translations I18n.locale, error_translation_key: 'Error message'
+      I18n.backend.store_translations I18n.locale, activerecord: {
+        errors: { models: { dummy_model: { attributes: { player: { should_play_in_any_club: 'Error message'} } } } }
+      }
     end
 
     describe "when exted a model" do
@@ -23,6 +25,10 @@ describe Extensions::GameEvent do
         subject { dummy }
 
         it { should be_valid }
+        it { should_not respond_to(:second_player_file) }
+        it { should_not respond_to(:same_player?) }
+        it { should_not respond_to(:same_club?) }
+        it { should_not respond_to(:validate_player_in_clubs) }
       end
 
       context "without game" do
@@ -66,7 +72,9 @@ describe Extensions::GameEvent do
         include Extensions::GameEvent
         acts_as_game_event player_relation: :scorer
       end
-      I18n.backend.store_translations I18n.locale, error_translation_key: 'Error message'
+      I18n.backend.store_translations I18n.locale, activerecord: {
+        errors: { models: { dummy_model: { attributes: { scorer: { should_play_in_any_club: 'Error message'} } } } }
+      }
     end
 
     describe "when exted a model" do
@@ -107,6 +115,63 @@ describe Extensions::GameEvent do
         it { should_not be_valid }
         it { should have(1).error_on(:scorer) }
         it { dummy.error_on(:scorer).should include I18n.t(error_translation_key) }
+      end
+    end
+  end
+
+  describe "without second player relation" do
+    let(:same_club_error_translation_key) { 'activerecord.errors.models.dummy_model.attributes.second_player.should_be_in_same_club' }
+    let(:different_player_error_translation_key) { 'activerecord.errors.models.dummy_model.attributes.second_player.should_be_diferent' }
+    before(:all) do
+      build_model :dummy_models do
+        integer :game_id
+        integer :player_id
+        integer :second_player_id
+
+        include Extensions::GameEvent
+        acts_as_game_event second_player_relation: :second_player
+      end
+      I18n.backend.store_translations I18n.locale, activerecord: {
+        errors: { models: { dummy_model: { attributes: { second_player: { should_be_in_same_club: 'Same club error message' } } } } }
+      }
+      I18n.backend.store_translations I18n.locale, activerecord: {
+        errors: { models: { dummy_model: { attributes: { second_player: { should_be_diferent: 'Different player error message' } } } } }
+      }
+    end
+
+    describe "when exted a model" do
+      let(:game) { create(:game) }
+      let(:player) { create(:player_with_club, player_club: game.club_home) }
+      let(:second_player) { create(:player_with_club, player_club: game.club_home) }
+
+      context "with correct data" do
+        let(:dummy) { DummyModel.new(game: game, player: player, second_player: second_player) }
+        subject { dummy }
+
+        it { should be_valid }
+        it { should respond_to(:second_player_file) }
+        it { should respond_to(:same_player?) }
+        it { should respond_to(:same_club?) }
+        it { should respond_to(:validate_player_in_clubs) }
+      end
+
+      context "with a second player who does not play in the same club than player" do
+        let(:second_player_different_club) { create(:player_with_club, player_club: game.club_away) }
+        let(:dummy) { DummyModel.new(game: game, player: player, second_player: second_player_different_club) }
+        subject { dummy }
+
+        it { should_not be_valid }
+        it { should have(1).error_on(:second_player) }
+        it { dummy.error_on(:second_player).should include I18n.t(same_club_error_translation_key) }
+      end
+
+      context "with a player as second player" do
+        let(:dummy) { DummyModel.new(game: game, player: player, second_player: player) }
+        subject { dummy }
+
+        it { should_not be_valid }
+        it { should have(1).error_on(:second_player) }
+        it { dummy.error_on(:second_player).should include I18n.t(different_player_error_translation_key) }
       end
     end
   end
