73fc7dcc9475938019c388493a2bb369c017d2b1
Ruben Sierra <maguilag@gmail.com>
2012-06-13 20:47:16 +0200

    Removed minute validations specs from goals.

--- spec/models/goal_spec.rb
+++ spec/models/goal_spec.rb
@@ -12,43 +12,6 @@ describe Goal do
       its(:second_player_relation) { should eql :assistant }
     end
 
-    context "without minute" do
-      let(:goal) { build(:goal, minute: nil) }
-      subject { goal }
-
-      it { should_not be_valid }
-      it { should have(2).error_on(:minute) }
-      it { goal.error_on(:minute).should include I18n.t('errors.messages.blank') }
-      it { goal.error_on(:minute).should include I18n.t('errors.messages.not_a_number') }
-    end
-
-    context "with minute less than 0" do
-      let(:goal) { build(:goal, minute: -1) }
-      subject { goal }
-
-      it { should_not be_valid }
-      it { should have(1).error_on(:minute) }
-      it { goal.error_on(:minute).should include I18n.t('errors.messages.greater_than_or_equal_to', :count => 0) }
-    end
-
-    context "with minute greater than 130" do
-      let(:goal) { build(:goal, minute: 131) }
-      subject { goal }
-
-      it { should_not be_valid }
-      it { should have(1).error_on(:minute) }
-      it { goal.error_on(:minute).should include I18n.t('errors.messages.less_than_or_equal_to', :count => 130) }
-    end
-
-    context "with not integer minute" do
-      let(:goal) { build(:goal, minute: 10.1) }
-      subject { goal }
-
-      it { should_not be_valid }
-      it { should have(1).error_on(:minute) }
-      it { goal.error_on(:minute).should include I18n.t('errors.messages.not_an_integer') }
-    end
-
     context "without kind" do
       let(:goal) { build(:goal, kind: nil) }
       subject { goal }
