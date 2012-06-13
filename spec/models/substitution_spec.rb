b9ec8f35da017fe5b4b4b396750127bd7f3aa9df
Ruben Sierra <maguilag@gmail.com>
2012-06-13 20:47:25 +0200

    Removed minute validations specs from substitutions.

--- spec/models/substitution_spec.rb
+++ spec/models/substitution_spec.rb
@@ -20,42 +20,5 @@ describe Substitution do
       it { should have(1).error_on(:player_in) }
       it { substitution.error_on(:player_in).should include I18n.t('errors.messages.blank') }
     end
-
-    context "without minute" do
-      let(:substitution) { build(:substitution, minute: nil) }
-      subject { substitution }
-
-      it { should_not be_valid }
-      it { should have(2).error_on(:minute) }
-      it { substitution.error_on(:minute).should include I18n.t('errors.messages.blank') }
-      it { substitution.error_on(:minute).should include I18n.t('errors.messages.not_a_number') }
-    end
-
-    context "with minute less than 0" do
-      let(:substitution) { build(:substitution, minute: -1) }
-      subject { substitution }
-
-      it { should_not be_valid }
-      it { should have(1).error_on(:minute) }
-      it { substitution.error_on(:minute).should include I18n.t('errors.messages.greater_than_or_equal_to', :count => 0) }
-    end
-
-    context "with minute greater than 130" do
-      let(:substitution) { build(:substitution, minute: 131) }
-      subject { substitution }
-
-      it { should_not be_valid }
-      it { should have(1).error_on(:minute) }
-      it { substitution.error_on(:minute).should include I18n.t('errors.messages.less_than_or_equal_to', :count => 130) }
-    end
-
-    context "with not integer minute" do
-      let(:substitution) { build(:substitution, minute: 10.1) }
-      subject { substitution }
-
-      it { should_not be_valid }
-      it { should have(1).error_on(:minute) }
-      it { substitution.error_on(:minute).should include I18n.t('errors.messages.not_an_integer') }
-    end
   end
 end
