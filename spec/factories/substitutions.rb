43c7736fb394ccd5ed6cc61a3f0cebdd44180bb4
Ruben Sierra <maguilag@gmail.com>
2012-06-24 20:10:52 +0200

    Added substitution in away club factory.

--- spec/factories/substitutions.rb
+++ spec/factories/substitutions.rb
@@ -6,5 +6,10 @@ FactoryGirl.define do
     player_out { create(:player_in_game, player_game: game) }
     player_in { create(:player_with_club, player_club: game.club_home) }
     minute 1
+
+    factory :substitution_away do
+      player_out { create(:player_in_game_away, player_game: game) }
+      player_in { create(:player_with_club, player_club: game.club_away) }
+    end
   end
 end
