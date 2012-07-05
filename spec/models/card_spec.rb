93ef6d9e57e226a38b2414b443258ff40cf4a493
Ruben Sierra <maguilag@gmail.com>
2012-07-05 23:24:25 +0200

    Cards callbacks for stats.

--- spec/models/card_spec.rb
+++ spec/models/card_spec.rb
@@ -2,13 +2,67 @@ require 'spec_helper'
 
 describe Card do
   describe "when create" do
-    context "with correct data" do
+    context "a yellow card with correct data" do
       let(:card) { create(:card) }
       subject { card }
 
       it { should be_valid }
       it { card.class.include?(Extensions::GameEvent).should be_true }
       its(:player_relation) { should eql :player }
+      its(:player) { should have(1).stats }
+
+      context "the player stats" do
+        let(:player_stat) { card.player.stats.season(card.game.season).week(card.game.week).first }
+        subject { player_stat }
+
+        its(:points) { should eql Lineup::STATS[:points] + card.card_stats[:points] }
+        its(:yellow_cards) { should eql card.card_stats[:yellow_cards] }
+        its(:red_cards) { should be_zero }
+      end
+    end
+
+    context "a red card with correct data" do
+      let(:card) { create(:red_card) }
+      subject { card }
+
+      it { should be_valid }
+      it { card.class.include?(Extensions::GameEvent).should be_true }
+      its(:player_relation) { should eql :player }
+      its(:player) { should have(1).stats }
+
+      context "the player stats" do
+        let(:player_stat) { card.player.stats.season(card.game.season).week(card.game.week).first }
+        subject { player_stat }
+
+        its(:points) { should eql Lineup::STATS[:points] + card.card_stats[:points] }
+        its(:red_cards) { should eql card.card_stats[:red_cards] }
+        its(:yellow_cards) { should be_zero }
+      end
+
+      describe "and calculate points for stats" do
+        context "without a yellow card" do
+          subject { card }
+
+          its(:red_card_points) { should eql -3 }
+        end
+
+        context "with a yellow card" do
+          let(:yellow_card) { create(:card, game: card.game, player: card.player) }
+          before { yellow_card; card }
+          subject { card }
+
+          its(:red_card_points) { should eql -2 }
+        end
+
+        context "with two yellow cards" do
+          let(:yellow_card) { create(:card, game: card.game, player: card.player) }
+          let(:second_yellow_card) { create(:card, game: card.game, player: card.player) }
+          before { yellow_card; second_yellow_card; card }
+          subject { card }
+
+          its(:red_card_points) { should eql -1 }
+        end
+      end
     end
 
     context "without kind" do
