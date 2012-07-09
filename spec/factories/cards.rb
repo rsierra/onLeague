5dedd9d0ce38ecf68dc46e63bc0cdf1d16d46acb
Ruben Sierra <maguilag@gmail.com>
2012-07-09 20:29:49 +0200

    Used traits on cards factories.

--- spec/factories/cards.rb
+++ spec/factories/cards.rb
@@ -7,9 +7,18 @@ FactoryGirl.define do
 
     minute 1
     kind Card.kind.values.first
+  end
 
-    factory :red_card do
-      kind Card.kind.values.last
+  trait :red do
+    minute 2
+    kind Card.kind.values.second
+
+    before(:create) do |card, evaluator|
+      create(:card, player: card.player, game: card.game)
     end
+  end
+
+  trait :direct_red do
+    kind Card.kind.values.last
   end
 end
