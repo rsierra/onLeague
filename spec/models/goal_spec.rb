cc791b9f60bc92b80f53d1b0875c9e4dbb5801f4
Ruben Sierra <maguilag@gmail.com>
2012-07-15 13:17:31 +0200

    Goal specs for stat callbacks.

--- spec/models/goal_spec.rb
+++ spec/models/goal_spec.rb
@@ -10,6 +10,87 @@ describe Goal do
       it { goal.class.include?(Extensions::GameEvent).should be_true }
       its(:player_relation) { should eql :scorer }
       its(:second_player_relation) { should eql :assistant }
+      its(:scorer) { should have(1).stats }
+
+      context "the player stats" do
+        let(:scorer_stat) { goal.scorer.stats.season(goal.game.season).week(goal.game.week).first }
+        subject { scorer_stat }
+
+        its(:points) { should eql 5 }
+        its(:points) { should eql Lineup::STATS[:points] + goal.scorer_stats[:points] }
+        its(:goals_scored) { should eql 1 }
+        its(:goals_scored) { should eql goal.scorer_stats[:goals_scored] }
+      end
+
+      describe "and change kind" do
+        let(:scorer_stat) { goal.scorer.stats.season(goal.game.season).week(goal.game.week).first }
+        before { goal.update_attributes(kind: new_kind)}
+        subject { scorer_stat }
+
+        context "to own" do
+          let(:new_kind) { 'own' }
+
+          its(:points) { should be_zero }
+          its(:points) { should eql Lineup::STATS[:points] + goal.scorer_stats[:points] }
+          its(:goals_scored) { should be_zero }
+        end
+
+        context "to penalty" do
+          let(:new_kind) { 'penalty' }
+
+          its(:points) { should eql 4 }
+          its(:points) { should eql Lineup::STATS[:points] + goal.scorer_stats[:points] }
+          its(:goals_scored) { should eql 1 }
+          its(:goals_scored) { should eql goal.scorer_stats[:goals_scored] }
+        end
+
+        context "to penalty_saved" do
+          let(:new_kind) { 'penalty_saved' }
+
+          its(:points) { should be_zero }
+          its(:points) { should eql Lineup::STATS[:points] + goal.scorer_stats[:points] }
+          its(:goals_scored) { should be_zero }
+        end
+
+        context "to penalty_out" do
+          let(:new_kind) { 'penalty_out' }
+
+          its(:points) { should eql -1 }
+          its(:points) { should eql Lineup::STATS[:points] + goal.scorer_stats[:points] }
+          its(:goals_scored) { should be_zero }
+        end
+      end
+
+      context "and assistant" do
+        let(:assistant) { create(:player_in_game, player_game: goal.game) }
+        let(:assistant_stat) { goal.assistant.stats.season(goal.game.season).week(goal.game.week).first }
+        before { goal.update_attributes(assistant: assistant) }
+        subject { assistant_stat }
+
+        its(:points) { should eql 3 }
+        its(:points) { should eql Lineup::STATS[:points] + goal.assistant_stats[:points] }
+        its(:assists) { should eql 1 }
+        its(:assists) { should eql goal.assistant_stats[:assists] }
+
+        context "and destroy" do
+          before { assistant_stat; goal.destroy }
+          subject { assistant_stat.reload }
+
+          its(:points) { should eql 2 }
+          its(:points) { should eql Lineup::STATS[:points] }
+          its(:assists) { should be_zero }
+        end
+      end
+
+      context "and destroy" do
+        let(:scorer_stat) { goal.scorer.stats.season(goal.game.season).week(goal.game.week).first }
+        before { scorer_stat; goal.destroy }
+        subject { scorer_stat.reload }
+
+        its(:points) { should eql 2 }
+        its(:points) { should eql Lineup::STATS[:points] }
+        its(:goals_scored) { should be_zero }
+      end
     end
 
     context "without kind" do
