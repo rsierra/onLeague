974d5f530bd7efd30ea07f5da068b51e89ab9c5f
Ruben Sierra <maguilag@gmail.com>
2012-07-15 13:15:21 +0200

    Goal callbacks for scorer stats.

--- app/models/goal.rb
+++ app/models/goal.rb
@@ -11,6 +11,9 @@ class Goal < ActiveRecord::Base
 
   scope :club, ->(club) { joins(:scorer => :club_files).where(club_files: {club_id: club}) }
 
+  before_save :update_scorer_stats, if: 'scorer_id_changed? || kind_changed?'
+  before_destroy :restore_stats
+
   def kind_enum
     Goal.kind.values
   end
@@ -21,5 +24,33 @@ class Goal < ActiveRecord::Base
 
   def goal_assistant_presence
     errors.add(:assistant, :should_not_be) unless assistant_id.blank?
+  end
+
+  def scorer_stats_by_kind(kind)
+    case kind
+      when 'penalty_out' then { points: -3 }
+      when 'penalty_saved' then { points: -2 }
+      when 'penalty' then { points: 2, goals_scored: 1 }
+      when 'own' then { points: -2 }
+      when 'regular' then { points: scorer.position_on_date(game.date) == 'forward' ? 2 : 3, goals_scored: 1 }
+      else {}
+    end
+  end
+
+  def scorer_stats
+    scorer_stats_by_kind(kind)
+  end
+
+  def scorer_stats_was
+    last_kind = kind_was.blank? ? self.kind : self.kind_was
+    scorer_stats_by_kind(last_kind)
+  end
+
+  def update_scorer_stats
+    Player.find(scorer_id_was).remove_stats(game_id, scorer_stats_was) unless scorer_id_was.blank?
+    scorer.update_stats(game_id, scorer_stats)
+  end
+  def restore_stats
+    scorer.remove_stats(game_id, scorer_stats)
   end
 end
