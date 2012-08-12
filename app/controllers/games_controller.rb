370684e0be5615b1a92929b1bbef2a5d1e429953
Ruben Sierra <maguilag@gmail.com>
2012-08-12 14:12:12 +0200

    We need week in games index view.

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,12 +1,12 @@
 class GamesController < ApplicationController
   def index
-    week = params[:week] || @current_league.week
+    @week = params[:week] || @current_league.week
     @season = params[:season] || @current_league.season
 
-    @weeks = Kaminari.paginate_array(@current_league.season_week_list(@season)).page(week).per(1)
+    @weeks = Kaminari.paginate_array(@current_league.season_week_list(@season)).page(@week).per(1)
     @seasons = @current_league.season_list
     @season = @season
-    @games = @current_league.games.season(@season).week(week).order(:date)
+    @games = @current_league.games.season(@season).week(@week).order(:date)
   end
 
   def show
