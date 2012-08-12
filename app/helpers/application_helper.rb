ac487514be692f7cc7cd47019f1a3bf5b6cf9c87
Ruben Sierra <maguilag@gmail.com>
2012-08-12 14:31:33 +0200

    New partial for footer and simple language selector.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,14 @@ module ApplicationHelper
   def l_with_timezone(datetime, options = {})
     l datetime.in_time_zone(t('datetime.timezone')), options
   end
+
+  def default_locale? locale
+    I18n.default_locale == locale
+  end
+
+  def language_selector
+    I18n.available_locales.map do |locale|
+      link_to(locale, default_locale?(locale) ? "/" : "/#{locale}")
+    end.join(' | ')
+  end
 end
