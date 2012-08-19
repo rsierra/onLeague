502ef107487232af0eceea9a9231ec121b109720
Ruben Sierra <maguilag@gmail.com>
2012-08-19 12:38:26 +0200

    Top players of current week.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,7 @@ class HomeController < ApplicationController
   def index
     @users = User.latest
     @top_club_files = ClubFile.of_clubs(@current_league.season_club_ids)
-      .order_by_points_on_season_week(@current_league.season, @current_league.week)
+      .order_by_points_on_season_week(@current_league.season, @current_league.current_week)
       .limit(5)
   end
 end
