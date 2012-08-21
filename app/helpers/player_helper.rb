6a020c332aa2d8a622026fe44f4f7ec88c03ef20
Ruben Sierra <maguilag@gmail.com>
2012-08-22 00:33:14 +0200

    Jersey helper with size.

--- app/helpers/player_helper.rb
+++ app/helpers/player_helper.rb
@@ -1,14 +1,14 @@
 module PlayerHelper
-  def jersey_with_number file
+  def jersey_with_number file, size = :thumb
     number_color = "#{file.club.number_color}"
     border_color = "#{file.club.border_color}"
     border_style =  "0.1em 0.1em #{border_color}," +
                     "-0.1em -0.1em #{border_color}," +
                     "0.1em -0.1em #{border_color}," +
                     "-0.1em 0.1em #{border_color}"
-    content_tag(:div, class: :jersey) do
+    content_tag(:div, class: "jersey-#{size}") do
       content_tag(:span, file.player.file.number, style: "color: #{number_color}; text-shadow: #{border_style}") +
-      image_tag(file.club.jersey.url(:thumb),
+      image_tag(file.club.jersey.url(size),
         alt: "#{file.club.name} #{Club.human_attribute_name(:jersey)}")
     end
   end
