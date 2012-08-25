f949c4741e6bdd457077d394e9ea4332230add73
Ruben Sierra <maguilag@gmail.com>
2012-08-25 18:18:21 +0200

    Added team editor js to precompile assets.

--- config/application.rb
+++ config/application.rb
@@ -56,6 +56,7 @@ module OnLeague
     config.assets.enabled = true
     # For heroku not access the DB or load models when precompiling your assets
     config.assets.initialize_on_precompile = false
+    config.assets.precompile += ['team_editor.js']
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
