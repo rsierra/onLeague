8bc997291b584ed1f5459cca955a8ff36e1973c3
Ruben Sierra <maguilag@gmail.com>
2012-08-25 17:59:31 +0200

    Controller methods for search, create file and destroy file remote actions.

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -1,5 +1,6 @@
 class TeamsController < ApplicationController
   before_filter :authenticate_user!
+  before_filter :only_xhr, only: [:search, :create_file, :destroy_file]
 
   def index
     @teams = current_user.teams.of_league(@current_league)
@@ -7,6 +8,8 @@ class TeamsController < ApplicationController
 
   def show
     @team = current_user.teams.of_league(@current_league).find(params[:id])
+    @team_file = @team.files.build
+    search_club_files
   end
 
   def new
@@ -26,6 +29,30 @@ class TeamsController < ApplicationController
     end
   end
 
+  def create_file
+    @team = current_user.teams.of_league(@current_league).find(params[:team_id])
+    club_file = ClubFile.active.current.find params[:club_file_id]
+    @team_file = @team.files.build(club_file.attributes_for_team_file)
+    @team_file.date_in = Date.today
+    search_club_files
+    if @team_file.save
+      @team_file = @team.files.build
+    end
+    render :show
+  end
+
+  def destroy_file
+    @team = current_user.teams.of_league(@current_league).find(params[:team_id])
+    if !@team.active && @team.files.destroy(params[:team_file_id])
+      flash[:notice] = I18n.t('flash.team_files.destroy_success')
+    else
+      flash[:error] = I18n.t('flash.team_files.destroy_error')
+    end
+    @team_file = @team.files.build
+    search_club_files
+    render :show
+  end
+
   def activate
     @team = current_user.teams.of_league(@current_league).find(params[:team_id])
     @team.activate
@@ -37,5 +64,31 @@ class TeamsController < ApplicationController
       flash[:error] = I18n.t('flash.teams.activation_error')
       redirect_to team_path(@team)
     end
+  end
+
+  def search
+    @team = current_user.teams.of_league(@current_league).find(params[:team_id])
+    search_club_files
+    render partial: 'update_find_file'
+  end
+
+  private
+
+  def search_club_files
+    # Fix for kaminary paginator params, you have to nullify params
+    # to get a clean url link
+    @paginator_params = {}
+    params.keys.each { |key| @paginator_params[key] = nil }
+    @paginator_params.merge!( 'action' => 'search', 'controller' => 'teams',
+      'team_id' => @team.slug, 'locale' => params[:locale], 'q' => params[:q])
+
+    @search = ClubFile.active.current.of_clubs(@current_league.season_club_ids)
+      .order_by_points_on_season(@current_league.season).order(:value)
+      .search(params[:q])
+    # There is a problem with kaminary and grouped scopes, becouse use count to get total pages
+    # and gets a hash count, so we calculate total pages by hand to pass in pagination helper
+    per_page = 5
+    @num_pages = ClubFile.active.current.of_clubs(@current_league.season_club_ids).search(params[:q]).result.count / per_page + 1
+    @club_files = @search.result.page(params[:page]).per(per_page)
   end
 end
