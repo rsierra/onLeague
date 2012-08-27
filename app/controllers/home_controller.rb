66eac7876208cc525a2fb26adbb42d4e029833d8
Ruben Sierra <maguilag@gmail.com>
2012-08-27 09:49:13 +0200

    Limit top players in home to current and active players.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,7 @@
 class HomeController < ApplicationController
   def index
     @users = User.latest
-    @top_players = ClubFile.of_clubs(@current_league.season_club_ids)
+    @top_players = ClubFile.active.current.of_clubs(@current_league.season_club_ids)
       .order_by_points_on_season_week(@current_league.season, @current_league.current_week)
       .limit(5)
     @top_teams = Team.where(league_id: @current_league, season: @current_league.season)
