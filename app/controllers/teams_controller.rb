6a30bf02656bade723546547fea083044c312a37
Ruben Sierra <maguilag@gmail.com>
2012-08-31 21:33:19 +0200

    Controller methods to control file cart in session.

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -1,6 +1,6 @@
 class TeamsController < ApplicationController
   before_filter :authenticate_user!
-  before_filter :only_xhr, only: [:search, :create_file, :destroy_file]
+  before_filter :only_xhr, only: [:search, :create_file, :destroy_file, :empty_cart, :checkout_cart]
 
   def index
     @teams = current_user.teams.of_league(@current_league)
@@ -32,20 +32,35 @@ class TeamsController < ApplicationController
   def create_file
     load_team params[:team_id]
     club_file = ClubFile.active.current.find params[:club_file_id]
-    @team_file = @team.files.build(club_file.attributes_for_team_file)
-    search_club_files
-    if @team_file.save
-      @team_file = @team.files.build
+
+    if @team.active
+      buy_player @team, club_file
+    else
+      @team_file = @team.files.build(club_file.attributes_for_team_file)
+      if @team_file.save
+        @team_file = @team.files.build
+      else
+        flash.now[:js_error] = I18n.t('flash.team_files.create_file_error')
+      end
     end
+    search_club_files
     render :show
   end
 
   def destroy_file
-    if !@team.active && @team.files.destroy(params[:team_file_id])
-      flash[:notice] = I18n.t('flash.team_files.destroy_success')
     load_team params[:team_id]
+    if @team.active
+      if @team.remaining_changes?
+        sell_player @team, TeamFile.current.find(params[:team_file_id])
+      else
+        flash.now[:js_error] = I18n.t('flash.team_files.sell_player_error')
+      end
     else
-      flash[:error] = I18n.t('flash.team_files.destroy_error')
+      if @team.files.destroy(params[:team_file_id])
+        flash.now[:js_notice] = I18n.t('flash.team_files.destroy_success')
+      else
+        flash.now[:js_error] = I18n.t('flash.team_files.destroy_error')
+      end
     end
     @team_file = @team.files.build
     search_club_files
@@ -71,6 +86,28 @@ class TeamsController < ApplicationController
     render partial: 'update_find_file'
   end
 
+  def empty_cart
+    load_team params[:team_id]
+    session[@team.slug] = @team.file_cart = nil
+    @team_file = @team.files.build
+    search_club_files
+    render :show
+  end
+
+  def checkout_cart
+    load_team params[:team_id]
+    checkout_error = @team.checkout_cart
+    if checkout_error.blank?
+      session[@team.slug] = @team.file_cart = nil
+      flash.now[:js_notice] = I18n.t('flash.teams.checkout_cart_success')
+    else
+      flash.now[:js_error] = "#{I18n.t('flash.teams.checkout_cart_error')}: #{checkout_error}"
+    end
+    @team_file = @team.files.build
+    search_club_files
+    render :show
+  end
+
   private
 
   def search_club_files
@@ -94,5 +131,17 @@ class TeamsController < ApplicationController
   def load_team id
     @team = current_user.teams.of_league(@current_league).find(id)
     @team.file_cart = session[@team.slug] if session[@team.slug]
+  end
+
+  def buy_player team, file
+    session[team.slug] ||= Utils::Teams::FileCart.new
+    session[team.slug].buy_player file
+    team.file_cart = session[team.slug]
+  end
+
+  def sell_player team, file
+    session[team.slug] ||= Utils::Teams::FileCart.new
+    session[team.slug].sell_player file
+    team.file_cart = session[team.slug]
   end
 end
