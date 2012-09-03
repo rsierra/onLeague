42921850c5ffb882aac8aa95ea7cfda090ea19dd
Ruben Sierra <maguilag@gmail.com>
2012-09-04 01:37:49 +0200

    Ruby 1.9.3 hash style.

--- app/models/club.rb
+++ app/models/club.rb
@@ -4,12 +4,12 @@ class Club < ActiveRecord::Base
   attr_accessor :border_color
 
   has_many :club_files
-  has_many :files, :class_name => 'ClubFile', :conditions => 'date_out is null', :order => :number
+  has_many :files, class_name: 'ClubFile', conditions: 'date_out is null', order: :number
   accepts_nested_attributes_for :files
   validates_associated :files
 
   translates :description
-  has_many :club_translations, :dependent => :destroy
+  has_many :club_translations, dependent: :destroy
   accepts_nested_attributes_for :club_translations
   validates_associated :club_translations
 
@@ -17,29 +17,29 @@ class Club < ActiveRecord::Base
   friendly_id :name, use: :slugged
 
   #attr_accessible :logo
-  has_attached_file :logo,  :styles => { :medium => "200x200>", :small => "100x100>", :thumb => "50x50#" },
-                            :default_url => "/assets/missings/:class/:attachment/:style/missing.png"
+  has_attached_file :logo,  styles: { medium: "200x200>", small: "100x100>", thumb: "50x50#" },
+                            default_url: "/assets/missings/:class/:attachment/:style/missing.png"
 
-  validates_attachment_content_type :logo, :content_type => /image/, :message => :incorrect_format
+  validates_attachment_content_type :logo, content_type: /image/, message: :incorrect_format
 
-  validates_attachment_size :logo, :less_than => 500.kilobytes
+  validates_attachment_size :logo, less_than: 500.kilobytes
 
   #attr_accessible :jersey
-  has_attached_file :jersey,  :styles => { :normal => "84x87#", :thumb => "38x39#" },
-                            :default_url => "/assets/missings/:class/:attachment/:style/missing.png"
+  has_attached_file :jersey,  styles: { normal: "84x87#", thumb: "38x39#" },
+                            default_url: "/assets/missings/:class/:attachment/:style/missing.png"
 
-  validates_attachment_content_type :jersey, :content_type => /image/, :message => :incorrect_format
+  validates_attachment_content_type :jersey, content_type: /image/, message: :incorrect_format
 
-  validates_attachment_size :jersey, :less_than => 500.kilobytes
+  validates_attachment_size :jersey, less_than: 500.kilobytes
 
-  validates :name,  :presence => true, :uniqueness => true,
-                    :length => { :maximum => 25 }
-  validates :short_name,  :presence => true,
-                          :length => { :maximum => 3 }
-  validates :description, :presence => true
-  validates :slug,  :presence => true, :uniqueness => true
+  validates :name,  presence: true, uniqueness: true,
+                    length: { maximum: 25 }
+  validates :short_name,  presence: true,
+                          length: { maximum: 3 }
+  validates :description, presence: true
+  validates :slug,  presence: true, uniqueness: true
 
-  validates :number_color, :presence => true
+  validates :number_color, presence: true
 
   after_create :default_values
   after_initialize :set_attributes
