eb73ae82b313dfc3d6b3a1edd991733bed2500c4
Ruben Sierra <maguilag@gmail.com>
2012-09-16 22:20:14 +0200

    New home index data and styles.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,9 @@
 class HomeController < ApplicationController
   def index
     @users = User.latest
+    @last_winner = Team.where(league_id: @current_league, season: @current_league.season)
+      .order_by_points_on_season_week(@current_league.season, @current_league.last_week)
+      .limit(1).first
     @top_players = ClubFile.active.current.of_clubs(@current_league.season_club_ids)
       .order_by_points_on_season_week(@current_league.season, @current_league.current_week)
       .limit(5)
