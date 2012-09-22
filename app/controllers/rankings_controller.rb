5588ee5cd69622854ca18f2d99b3f2586cccc966
Ruben Sierra <maguilag@gmail.com>
2012-09-22 18:24:12 +0200

    Rankings params for providers.

--- app/controllers/rankings_controller.rb
+++ app/controllers/rankings_controller.rb
@@ -2,7 +2,17 @@ class RankingsController < ApplicationController
   def index
     @season = params[:season] || @current_league.season
 
+    @ranking_kind = params[:week] ? 'weekly' : 'seasonal'
+    @ranking_kind += "_#{params[:provider]}" if params[:provider]
+
     @rankings = Team.where(league_id: @current_league, season: @season)
+    if @current_user && params[:provider]
+      @provider = params[:provider]
+      provider_conditions = { provider: @provider, uid: @current_user.provider_friends(@provider) }
+      user_ids = User.select('users.id').joins(:oauth_providers).where(oauth_providers: provider_conditions).map(&:id)
+      user_ids << @current_user.id
+      @rankings = @rankings.of_user(user_ids)
+    end
     @rankings = unless params[:week]
       @rankings.order_by_points_on_season(@season)
     else
