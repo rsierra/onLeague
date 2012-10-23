39d41d395085e1e0a3d4c061a33bf9a0d32106ca
Ruben Sierra <maguilag@gmail.com>
2012-10-23 20:47:42 +0200

    Html format added to mailer templates.

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,13 +2,14 @@ class UserMailer < ActionMailer::Base
   default :from => "no-replay@onleague.org"
 
   def points_notification(team)
+    @team = team
     @user_name = team.user.name
     @week = team.league.last_week
+    @season = team.league.season
     @league_name = team.league.name
-    @team_name = team.name
     @points = team.season_week_points(team.league.season, @week)
     mail(:to => team.user.email,
-         :subject => I18n.t('points_notification_subject', scope: "mailers.user_mailers", team_name: @team_name))
+         :subject => I18n.t('points_notification_subject', scope: "mailers.user_mailers", team_name: team.name))
   end
 
   def inactive_notification(team)
@@ -17,7 +18,7 @@ class UserMailer < ActionMailer::Base
     @week = team.league.last_week
     @league_name = team.league.name
     mail(:to => team.user.email,
-         :subject => I18n.t('inactive_notification_subject', scope: "mailers.user_mailers", team_name: @team.name))
+         :subject => I18n.t('inactive_notification_subject', scope: "mailers.user_mailers", team_name: team.name))
   end
 
 end
