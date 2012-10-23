3a92bd9e7ce2706f6f3a251fae2d35fb0e196729
Ruben Sierra <maguilag@gmail.com>
2012-10-23 20:48:56 +0200

    Redis configuration different for production and another environments.

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -6,4 +6,9 @@ OnLeague::Application.config.session_store :cookie_store, key: '_onLeague_sessio
 # which shouldn't be used to store highly confidential information
 # (create the session table with "rails generate session_migration")
 # OnLeague::Application.config.session_store :active_record_store
-OnLeague::Application.config.session_store :redis_store, servers: { path: "#{ENV['OPENSHIFT_GEAR_DIR']}tmp/redis.sock" }
+if Rails.env.production?
+  OnLeague::Application.config.session_store :redis_store, servers: { path: "#{ENV['OPENSHIFT_GEAR_DIR']}tmp/redis.sock" }
+else
+  OnLeague::Application.config.session_store :redis_store, servers: ENV['REDISTOGO_URL']
+end
+
