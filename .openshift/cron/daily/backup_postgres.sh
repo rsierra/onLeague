dd8f309141200ffaff44b3ed39b528c04d9f4cd3
Ruben Sierra <maguilag@gmail.com>
2013-02-26 21:53:51 +0100

    Fixed problem with daily backups

--- .openshift/cron/daily/backup_postgres.sh
+++ .openshift/cron/daily/backup_postgres.sh
@@ -4,4 +4,4 @@
 
 NOW="$(date +"%Y-%m-%d")"
 FILENAME="$OPENSHIFT_DATA_DIR/db_backups/$OPENSHIFT_APP_NAME.$NOW.backup.sql.gz"
-pd_dump $OPENSHIFT_APP_NAME | gzip > $FILENAME
+pg_dump -U $OPENSHIFT_DB_USERNAME -h $OPENSHIFT_DB_HOST -w $OPENSHIFT_APP_NAME | gzip > $FILENAME
